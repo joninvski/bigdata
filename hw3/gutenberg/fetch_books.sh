8b131accec109ebbb3d8c573069bf976491583e9
Joao Trindade <trindade.joao@gmail.com>
2013-09-17 23:20:41 +0100

    Initial commit

---
+++ hw3/gutenberg/fetch_books.sh
@@ -0,0 +1,4 @@
+wget "http://www.gutenberg.org/cache/epub/1661/pg1661.txt" -O sherlock.txt
+wget "http://www.gutenberg.org/ebooks/16328.txt.utf-8" -O beowulf.txt
+wget "http://www.gutenberg.org/ebooks/1232.txt.utf-8" -O the_prince.txt
+wget "http://www.gutenberg.org/ebooks/135.txt.utf-8" -O les_miserable.txt
