e5cfa172c07e1f8b7def688b176b90db190eb559
Joao Trindade <trindade.joao@gmail.com>
2013-11-01 20:13:09 +0000

    Doing hw6

--- hw6/bayesian.py
+++ hw6/bayesian.py
@@ -1,30 +1,177 @@
 import sqlite3
 
-homework = sqlite3.connect(':memory:')
-c = homework.cursor()
 
 # Create database for Bayesian network (page 16 of the slides)
 
-c.execute('CREATE TABLE rain (R text, P float)')
-c.execute('INSERT INTO rain VALUES ("Y", 0.2)')
-c.execute('INSERT INTO rain VALUES ("N", 0.8)')
-homework.commit()
+def create_database():
+    db = sqlite3.connect(':memory:')
+    return db
 
-c.execute('CREATE TABLE wet (W text, R text, P float)')
-c.execute('INSERT INTO wet VALUES ("Y", "Y", 0.90)')
-c.execute('INSERT INTO wet VALUES ("N", "Y", 0.10)')
-c.execute('INSERT INTO wet VALUES ("Y", "N", 0.20)')
-c.execute('INSERT INTO wet VALUES ("N", "N", 0.80)')
-homework.commit()
 
-# SQL query
-c.execute('SELECT rain.R, SUM(rain.P * wet.P) \
-           FROM rain, wet \
-           WHERE wet.W="Y" AND rain.R=wet.R \
-           GROUP BY rain.R')
 
-result = c.fetchall()
+
+
+
+
+def insert_values(db):
+    c = db.cursor()
+
+    # --------------
+    # |    Asia    |
+    # --------------
+    # | Y   | 0.01 |
+    # | N   | 0.99 |
+    # --------------
+    c.execute('CREATE TABLE Asia (A text, P float)')
+    c.execute('INSERT INTO Asia VALUES ("Y", 0.01)')
+    c.execute('INSERT INTO Asia VALUES ("N", 0.99)')
+    db.commit()
+
+    # ------------------------------
+    # | Tuberculosis | Asia | %    |
+    # | Y            | Y    | 0.05 |
+    # | Y            | N    | 0.01 |
+    # | N            | Y    | 0.95 |
+    # | N            | N    | 0.99 |
+    # ------------------------------
+    c.execute('CREATE TABLE Tuberculosis (T text, A text, P float)')
+    c.execute('INSERT INTO Tuberculosis VALUES ("Y", "Y", 0.05)')
+    c.execute('INSERT INTO Tuberculosis VALUES ("N", "Y", 0.01)')
+    c.execute('INSERT INTO Tuberculosis VALUES ("Y", "N", 0.95)')
+    c.execute('INSERT INTO Tuberculosis VALUES ("N", "N", 0.99)')
+    db.commit()
+
+    # ------------------
+    # | Smoking | %    |
+    # | Y       | 0.50 |
+    # | N       | 0.50 |
+    # ------------------
+    c.execute('CREATE TABLE Smoking(S text, P float)')
+    c.execute('INSERT INTO Smoking VALUES ("Y", 0.50)')
+    c.execute('INSERT INTO Smoking VALUES ("N", 0.50)')
+    db.commit()
+
+    # -------------------------------
+    # | Lungcancer | Smoking | %    |
+    # | Y          | Y       | 0.10 |
+    # | Y          | N       | 0.01 |
+    # | N          | Y       | 0.90 |
+    # | N          | N       | 0.10 |
+    # -------------------------------
+    c.execute('CREATE TABLE Lungcancer(L text, S text, P float)')
+    c.execute('INSERT INTO Lungcancer VALUES ("Y", "Y", 0.10)')
+    c.execute('INSERT INTO Lungcancer VALUES ("N", "Y", 0.01)')
+    c.execute('INSERT INTO Lungcancer VALUES ("Y", "N", 0.90)')
+    c.execute('INSERT INTO Lungcancer VALUES ("N", "N", 0.10)')
+    db.commit()
+
+    # -------------------------------
+    # | Bronchitis | Smoking | %    |
+    # | Y          | Y       | 0.60 |
+    # | Y          | N       | 0.30 |
+    # | N          | Y       | 0.40 |
+    # | N          | N       | 0.70 |
+    # -------------------------------
+    c.execute('CREATE TABLE Bronchitis(B text, S text, P float)')
+    c.execute('INSERT INTO Bronchitis VALUES ("Y", "Y", 0.60)')
+    c.execute('INSERT INTO Bronchitis VALUES ("N", "Y", 0.30)')
+    c.execute('INSERT INTO Bronchitis VALUES ("Y", "N", 0.40)')
+    c.execute('INSERT INTO Bronchitis VALUES ("N", "N", 0.70)')
+    db.commit()
+
+    # ---------------------------------------------------------
+    # | EitherTubOrLungCancer | LungCancer | Tuberculosis | % |
+    # | Y                     | Y          | Y            | 1 |
+    # | Y                     | Y          | N            | 1 |
+    # | Y                     | N          | Y            | 1 |
+    # | Y                     | N          | N            | 0 |
+    # | N                     | Y          | Y            | 0 |
+    # | N                     | Y          | N            | 0 |
+    # | N                     | N          | Y            | 0 |
+    # | N                     | N          | N            | 1 |
+    # ---------------------------------------------------------
+    c.execute('CREATE TABLE EitherTubOrLungCancer(E text, T text, L text, P float)')
+    c.execute('INSERT INTO EitherTubOrLungCancer VALUES ("Y", "Y", "Y", 1)')
+    c.execute('INSERT INTO EitherTubOrLungCancer VALUES ("Y", "Y", "N", 1)')
+    c.execute('INSERT INTO EitherTubOrLungCancer VALUES ("Y", "N", "Y", 1)')
+    c.execute('INSERT INTO EitherTubOrLungCancer VALUES ("Y", "N", "N", 0)')
+    c.execute('INSERT INTO EitherTubOrLungCancer VALUES ("N", "Y", "Y", 0)')
+    c.execute('INSERT INTO EitherTubOrLungCancer VALUES ("N", "Y", "N", 0)')
+    c.execute('INSERT INTO EitherTubOrLungCancer VALUES ("N", "N", "Y", 0)')
+    c.execute('INSERT INTO EitherTubOrLungCancer VALUES ("N", "N", "N", 1)')
+
+    # ---------------------------------------
+    # | Xray | EitherTubOrLungCancer | %    |
+    # | Y    | Y                     | 0.98 |
+    # | Y    | N                     | 0.05 |
+    # | N    | Y                     | 0.02 |
+    # | N    | N                     | 0.95 |
+    # ---------------------------------------
+    c.execute('CREATE TABLE Xray (X text, E text, P float)')
+    c.execute('INSERT INTO Xray VALUES ("Y", "Y", 0.98)')
+    c.execute('INSERT INTO Xray VALUES ("Y", "N", 0.05)')
+    c.execute('INSERT INTO Xray VALUES ("N", "Y", 0.02)')
+    c.execute('INSERT INTO Xray VALUES ("N", "N", 0.95)')
+
+    # --------------------------------------------------------
+    # | Dyspnoea | EitherTubOrLungCancer | Bronchitis | %    |
+    # | Y        | Y                     | Y          | 0.90 |
+    # | Y        | Y                     | N          | 0.70 |
+    # | Y        | N                     | Y          | 0.80 |
+    # | Y        | N                     | N          | 0.10 |
+    # | N        | Y                     | Y          | 0.10 |
+    # | N        | Y                     | N          | 0.30 |
+    # | N        | N                     | Y          | 0.20 |
+    # | N        | N                     | N          | 0.90 |
+    # --------------------------------------------------------
+    c.execute('CREATE TABLE Dyspnoea (D text, E text, B text, P float)')
+    c.execute('INSERT INTO Dyspnoea VALUES ("Y", "Y", "Y", 0.90)')
+    c.execute('INSERT INTO Dyspnoea VALUES ("Y", "Y", "N", 0.70)')
+    c.execute('INSERT INTO Dyspnoea VALUES ("Y", "N", "Y", 0.80)')
+    c.execute('INSERT INTO Dyspnoea VALUES ("Y", "N", "N", 0.10)')
+    c.execute('INSERT INTO Dyspnoea VALUES ("N", "Y", "Y", 0.10)')
+    c.execute('INSERT INTO Dyspnoea VALUES ("N", "Y", "N", 0.30)')
+    c.execute('INSERT INTO Dyspnoea VALUES ("N", "N", "Y", 0.20)')
+    c.execute('INSERT INTO Dyspnoea VALUES ("N", "N", "N", 0.90)')
+
+
+def ask_question(db):
+    c = db.cursor()
+    # SQL query
+    c.execute('SELECT rain.R, SUM(rain.P * wet.P) \
+               FROM rain, wet \
+               WHERE wet.W="Y" AND rain.R=wet.R \
+               GROUP BY rain.R')
+    result = c.fetchall()
+    return result
+
+def test_question(db):
+    # patient who did not go to Asia, did not smoke, did not have a positive X-ray and did not have dyspnoea
+    # tuberculosis
+    # yes: 0.00007470 no: 0.99992530
+    # lung cancer
+    # yes: 0.00007470 no: 0.99992530
+    # bronchitis
+    # yes: 0.08696218 no: 0.91303782
+    c = db.cursor()
+    c.execute('SELECT Tuberculosis.T, SUM(Tuberculosis.P * Asia.P) \
+               FROM Tuberculosis, Asia\
+               WHERE Asia.A="N" AND Tuberculosis.A=Asia.A \
+               GROUP BY Tuberculosis.T')
+    result = c.fetchall()
+    return result
+
+
+db = create_database()
+insert_values(db)
+result = test_question(db)
+# result = ask_question(db)
+
 print result
+print ""
+
 n = result[0][1]
 y = result[1][1]
-print y / (y + n)
+
+print "Ponderated result (Y) = " + str(y / (y + n))
+print "Ponderated result (N) = " + str(n / (y + n))
---
+++ hw6/example.py
@@ -0,0 +1,150 @@
+import sqlite3
+
+
+# Create database for Bayesian network (page 16 of the slides)
+
+def create_database():
+    db = sqlite3.connect(':memory:')
+    return db
+
+# Original
+# +------+     +-----+
+# | Rain | --> | Wet |
+# +------+     +-----+
+
+# Now with thunder
+# +------+     +---------+
+# | Rain | --> | Thunder |
+# +------+     +---------+
+#   |
+#   |
+#   v
+# +------+
+# | Wet  |
+# +------+
+
+# Now with sprinkler and hose
+# +------+     +-----------+
+# | Rain | --> |  Thunder  |
+# +------+     +-----------+
+#   |
+#   |
+#   v
+# +------+     +-----------+     +------+
+# | Wet  | <-- | Sprinkler | --> | Hose |
+# +------+     +-----------+     +------+
+#   ^            |
+#   +------------+
+
+
+# --------------
+# |    RAIN    |
+# --------------
+# | Y    | 0.2 |
+# | N    | 0.8 |
+# --------------
+
+# ----------------
+# | Wet  |  Rain |
+# ----------------
+# | Y | Y | 0.90 |
+# | Y | N | 0.20 |
+# | N | Y | 0.10 |
+# | N | N | 0.80 |
+# ----------------
+
+# ------------------------
+# | Thunder | Rain | %   |
+# | Y       | Y    | 0.8 |
+# | Y       | N    | 0.1 |
+# | N       | Y    | 0.2 |
+# | N       | N    | 0.9 |
+# ------------------------
+
+
+def insert_values(db):
+    c = db.cursor()
+    c.execute('CREATE TABLE rain (R text, P float)')
+    c.execute('INSERT INTO rain VALUES ("Y", 0.2)')
+    c.execute('INSERT INTO rain VALUES ("N", 0.8)')
+    db.commit()
+
+    c.execute('CREATE TABLE wet (W text, R text, P float)')
+    c.execute('INSERT INTO wet VALUES ("Y", "Y", 0.90)')
+    c.execute('INSERT INTO wet VALUES ("Y", "N", 0.20)')
+    c.execute('INSERT INTO wet VALUES ("N", "Y", 0.10)')
+    c.execute('INSERT INTO wet VALUES ("N", "N", 0.80)')
+    db.commit()
+
+    # Slide 21
+    c.execute('CREATE TABLE newWet (W text, S text, R text, P float)')
+    c.execute('INSERT INTO newWet VALUES ("Y", "Y", "Y", 0.90)')
+    c.execute('INSERT INTO newWet VALUES ("Y", "Y", "N", 0.70)')
+    c.execute('INSERT INTO newWet VALUES ("Y", "N", "Y", 0.80)')
+    c.execute('INSERT INTO newWet VALUES ("Y", "N", "N", 0.10)')
+    c.execute('INSERT INTO newWet VALUES ("N", "Y", "Y", 0.10)')
+    c.execute('INSERT INTO newWet VALUES ("N", "Y", "N", 0.30)')
+    c.execute('INSERT INTO newWet VALUES ("N", "N", "Y", 0.20)')
+    c.execute('INSERT INTO newWet VALUES ("N", "N", "N", 0.90)')
+    db.commit()
+
+    c.execute('CREATE TABLE thunder (T text, R text, P float)')
+    c.execute('INSERT INTO thunder VALUES ("Y", "Y", 0.80)')
+    c.execute('INSERT INTO thunder VALUES ("Y", "N", 0.10)')
+    c.execute('INSERT INTO thunder VALUES ("N", "Y", 0.20)')
+    c.execute('INSERT INTO thunder VALUES ("N", "N", 0.90)')
+    db.commit()
+
+    c.execute('CREATE TABLE sprinkler (S text, P text)')
+    c.execute('INSERT INTO sprinkler VALUES ("Y", 0.3)')
+    c.execute('INSERT INTO sprinkler VALUES ("N", 0.7)')
+    db.commit()
+
+
+def ask_question_knowing_wet_but_dont_know_sprinkler(db):
+    c = db.cursor()
+    c.execute('SELECT rain.R, newWet.P, sprinkler.P\
+               FROM rain, newWet, sprinkler\
+               WHERE newWet.W = "Y" and rain.R = newWet.R and sprinkler.S = newWet.S\
+               GROUP BY rain.R\
+               ')
+    result = c.fetchall()
+    return result
+
+def ask_question_knowing_wet_and_thunder(db):
+    c = db.cursor()
+    c.execute('SELECT rain.R, SUM(rain.P * wet.P * thunder.P)\
+               FROM rain, wet, thunder\
+               WHERE thunder.T = "Y" AND wet.W = "Y" and rain.R = wet.R and wet.R = thunder.R\
+               GROUP BY rain.R\
+               ')
+    result = c.fetchall()
+    return result
+
+def ask_question_knowing_wet(db):
+    c = db.cursor()
+    # Question is probability of raining knowing the ground is wet
+    c.execute('SELECT rain.R, SUM(rain.P * wet.P) \
+               FROM rain, wet \
+               WHERE wet.W="Y" AND rain.R=wet.R \
+               GROUP BY rain.R')
+    result = c.fetchall()
+    return result
+print "######################################"
+
+db = create_database()
+insert_values(db)
+result = ask_question_knowing_wet(db)
+result = ask_question_knowing_wet_and_thunder(db)
+result = ask_question_knowing_wet_but_dont_know_sprinkler(db)
+
+print "Result: "
+for p in result:
+    print p
+print ""
+
+n = result[0][1]
+y = result[1][1]
+
+print "Ponderated result (Y) = " + str(y / (y + n))
+print "Ponderated result (N) = " + str(n / (y + n))
