c269e62131e031e8534c3ca31185e7dcd0f79ebb
Joao Trindade <trindade.joao@gmail.com>
2013-11-02 16:59:16 +0000

    Works. Ugly but works

--- hw6/bayesian.py
+++ hw6/bayesian.py
@@ -36,8 +36,8 @@ def insert_values(db):
     # ------------------------------
     c.execute('CREATE TABLE Tuberculosis (T text, A text, P float)')
     c.execute('INSERT INTO Tuberculosis VALUES ("Y", "Y", 0.05)')
-    c.execute('INSERT INTO Tuberculosis VALUES ("N", "Y", 0.01)')
-    c.execute('INSERT INTO Tuberculosis VALUES ("Y", "N", 0.95)')
+    c.execute('INSERT INTO Tuberculosis VALUES ("Y", "N", 0.01)')
+    c.execute('INSERT INTO Tuberculosis VALUES ("N", "Y", 0.95)')
     c.execute('INSERT INTO Tuberculosis VALUES ("N", "N", 0.99)')
     db.commit()
 
@@ -60,9 +60,9 @@ def insert_values(db):
     # -------------------------------
     c.execute('CREATE TABLE Lungcancer(L text, S text, P float)')
     c.execute('INSERT INTO Lungcancer VALUES ("Y", "Y", 0.10)')
-    c.execute('INSERT INTO Lungcancer VALUES ("N", "Y", 0.01)')
-    c.execute('INSERT INTO Lungcancer VALUES ("Y", "N", 0.90)')
-    c.execute('INSERT INTO Lungcancer VALUES ("N", "N", 0.10)')
+    c.execute('INSERT INTO Lungcancer VALUES ("Y", "N", 0.01)')
+    c.execute('INSERT INTO Lungcancer VALUES ("N", "Y", 0.90)')
+    c.execute('INSERT INTO Lungcancer VALUES ("N", "N", 0.99)')
     db.commit()
 
     # -------------------------------
@@ -74,13 +74,13 @@ def insert_values(db):
     # -------------------------------
     c.execute('CREATE TABLE Bronchitis(B text, S text, P float)')
     c.execute('INSERT INTO Bronchitis VALUES ("Y", "Y", 0.60)')
-    c.execute('INSERT INTO Bronchitis VALUES ("N", "Y", 0.30)')
-    c.execute('INSERT INTO Bronchitis VALUES ("Y", "N", 0.40)')
+    c.execute('INSERT INTO Bronchitis VALUES ("Y", "N", 0.30)')
+    c.execute('INSERT INTO Bronchitis VALUES ("N", "Y", 0.40)')
     c.execute('INSERT INTO Bronchitis VALUES ("N", "N", 0.70)')
     db.commit()
 
     # ---------------------------------------------------------
-    # | EitherTubOrLungCancer | LungCancer | Tuberculosis | % |
+    # | EitherTubOrLungcancer | Lungcancer | Tuberculosis | % |
     # | Y                     | Y          | Y            | 1 |
     # | Y                     | Y          | N            | 1 |
     # | Y                     | N          | Y            | 1 |
@@ -90,18 +90,18 @@ def insert_values(db):
     # | N                     | N          | Y            | 0 |
     # | N                     | N          | N            | 1 |
     # ---------------------------------------------------------
-    c.execute('CREATE TABLE EitherTubOrLungCancer(E text, T text, L text, P float)')
-    c.execute('INSERT INTO EitherTubOrLungCancer VALUES ("Y", "Y", "Y", 1)')
-    c.execute('INSERT INTO EitherTubOrLungCancer VALUES ("Y", "Y", "N", 1)')
-    c.execute('INSERT INTO EitherTubOrLungCancer VALUES ("Y", "N", "Y", 1)')
-    c.execute('INSERT INTO EitherTubOrLungCancer VALUES ("Y", "N", "N", 0)')
-    c.execute('INSERT INTO EitherTubOrLungCancer VALUES ("N", "Y", "Y", 0)')
-    c.execute('INSERT INTO EitherTubOrLungCancer VALUES ("N", "Y", "N", 0)')
-    c.execute('INSERT INTO EitherTubOrLungCancer VALUES ("N", "N", "Y", 0)')
-    c.execute('INSERT INTO EitherTubOrLungCancer VALUES ("N", "N", "N", 1)')
+    c.execute('CREATE TABLE EitherTubOrLungcancer(E text, T text, L text, P float)')
+    c.execute('INSERT INTO EitherTubOrLungcancer VALUES ("Y", "Y", "Y", 1)')
+    c.execute('INSERT INTO EitherTubOrLungcancer VALUES ("Y", "Y", "N", 1)')
+    c.execute('INSERT INTO EitherTubOrLungcancer VALUES ("Y", "N", "Y", 1)')
+    c.execute('INSERT INTO EitherTubOrLungcancer VALUES ("Y", "N", "N", 0)')
+    c.execute('INSERT INTO EitherTubOrLungcancer VALUES ("N", "Y", "Y", 0)')
+    c.execute('INSERT INTO EitherTubOrLungcancer VALUES ("N", "Y", "N", 0)')
+    c.execute('INSERT INTO EitherTubOrLungcancer VALUES ("N", "N", "Y", 0)')
+    c.execute('INSERT INTO EitherTubOrLungcancer VALUES ("N", "N", "N", 1)')
 
     # ---------------------------------------
-    # | Xray | EitherTubOrLungCancer | %    |
+    # | Xray | EitherTubOrLungcancer | %    |
     # | Y    | Y                     | 0.98 |
     # | Y    | N                     | 0.05 |
     # | N    | Y                     | 0.02 |
@@ -114,7 +114,7 @@ def insert_values(db):
     c.execute('INSERT INTO Xray VALUES ("N", "N", 0.95)')
 
     # --------------------------------------------------------
-    # | Dyspnoea | EitherTubOrLungCancer | Bronchitis | %    |
+    # | Dyspnoea | EitherTubOrLungcancer | Bronchitis | %    |
     # | Y        | Y                     | Y          | 0.90 |
     # | Y        | Y                     | N          | 0.70 |
     # | Y        | N                     | Y          | 0.80 |
@@ -135,43 +135,118 @@ def insert_values(db):
     c.execute('INSERT INTO Dyspnoea VALUES ("N", "N", "N", 0.90)')
 
 
-def ask_question(db):
-    c = db.cursor()
-    # SQL query
-    c.execute('SELECT rain.R, SUM(rain.P * wet.P) \
-               FROM rain, wet \
-               WHERE wet.W="Y" AND rain.R=wet.R \
-               GROUP BY rain.R')
-    result = c.fetchall()
-    return result
-
-def test_question(db):
-    # patient who did not go to Asia, did not smoke, did not have a positive X-ray and did not have dyspnoea
+def test_question_tuberculosis(db):
+    # patient who did not go to Asia, did not smoke, did not have a positive X-ray and did not have Dyspnoea
     # tuberculosis
     # yes: 0.00007470 no: 0.99992530
+    cr = db.cursor()
+
+            # SELECT Tuberculosis.T HasT, Tuberculosis.P TuberP, Asia.P AsiaP, EitherTubOrLungcancer.P EitherP, Xray.P XrayP, Smoking.P Smoke, Lungcancer.P Lung,  Bronchitis.P Bronc,  Dyspnoea.P Dyspnoea \
+    cr.execute('\
+            SELECT Tuberculosis.T HasT, SUM(Tuberculosis.P * Asia.P * EitherTubOrLungcancer.P * Xray.P * Smoking.P * Lungcancer.P *  Bronchitis.P * Dyspnoea.P ) \
+            FROM Tuberculosis, Asia, EitherTubOrLungcancer, Xray, Smoking, Lungcancer, Bronchitis, Dyspnoea\
+            WHERE \
+            Asia.A = "N" \
+            AND Smoking.S = "N" \
+            AND Xray.X = "N" \
+            AND Dyspnoea.D = "N" \
+            AND Asia.A = Tuberculosis.A \
+            AND Smoking.S = Lungcancer.S \
+            AND Smoking.S = Bronchitis.S \
+            AND Tuberculosis.T = EitherTubOrLungcancer.T \
+            AND Lungcancer.L = EitherTubOrLungcancer.L \
+            AND Bronchitis.B = Dyspnoea.B \
+            AND EitherTubOrLungcancer.E = Dyspnoea.E \
+            AND EitherTubOrLungcancer.E = Xray.E \
+            GROUP BY HasT \
+            ORDER BY HasT' \
+            )
+
+    result = cr.fetchall()
+    desc = [d[0] for d in cr.description]
+    return (desc, result)
+
+def test_question_lung_cancer(db):
     # lung cancer
     # yes: 0.00007470 no: 0.99992530
-    # bronchitis
+    cr = db.cursor()
+
+    # SELECT Tuberculosis.T HasT, Tuberculosis.P TuberP, Asia.P AsiaP, EitherTubOrLungcancer.P EitherP, Xray.P XrayP, Smoking.P Smoke, Lungcancer.P Lung,  Bronchitis.P Bronc,  Dyspnoea.P Dyspnoea \
+    cr.execute('\
+        SELECT Lungcancer.L HasL, SUM(Tuberculosis.P * Asia.P * EitherTubOrLungcancer.P * Xray.P * Smoking.P * Lungcancer.P *  Bronchitis.P * Dyspnoea.P ) \
+        FROM Tuberculosis, Asia, EitherTubOrLungcancer, Xray, Smoking, Lungcancer, Bronchitis, Dyspnoea\
+        WHERE \
+        Asia.A = "N" \
+        AND Smoking.S = "N" \
+        AND Xray.X = "N" \
+        AND Dyspnoea.D = "N" \
+        AND Asia.A = Tuberculosis.A \
+        AND Smoking.S = Lungcancer.S \
+        AND Smoking.S = Bronchitis.S \
+        AND Tuberculosis.T = EitherTubOrLungcancer.T \
+        AND Lungcancer.L = EitherTubOrLungcancer.L \
+        AND Bronchitis.B = Dyspnoea.B \
+        AND EitherTubOrLungcancer.E = Dyspnoea.E \
+        AND EitherTubOrLungcancer.E = Xray.E \
+        GROUP BY HasL \
+        ORDER BY HasL' \
+        )
+
+    result = cr.fetchall()
+    desc = [d[0] for d in cr.description]
+    return (desc, result)
+
+
+def test_question_bronchitis(db):
+    # Bronchitis
     # yes: 0.08696218 no: 0.91303782
-    c = db.cursor()
-    c.execute('SELECT Tuberculosis.T, SUM(Tuberculosis.P * Asia.P) \
-               FROM Tuberculosis, Asia\
-               WHERE Asia.A="N" AND Tuberculosis.A=Asia.A \
-               GROUP BY Tuberculosis.T')
-    result = c.fetchall()
-    return result
+    cr = db.cursor()
+
+    # SELECT Tuberculosis.T HasT, Tuberculosis.P TuberP, Asia.P AsiaP, EitherTubOrLungcancer.P EitherP, Xray.P XrayP, Smoking.P Smoke, Lungcancer.P Lung,  Bronchitis.P Bronc,  Dyspnoea.P Dyspnoea \
+    cr.execute('\
+        SELECT Bronchitis.B HasB, SUM(Tuberculosis.P * Asia.P * EitherTubOrLungcancer.P * Xray.P * Smoking.P * Lungcancer.P *  Bronchitis.P * Dyspnoea.P ) \
+        FROM Tuberculosis, Asia, EitherTubOrLungcancer, Xray, Smoking, Lungcancer, Bronchitis, Dyspnoea\
+        WHERE \
+        Asia.A = "N" \
+        AND Smoking.S = "N" \
+        AND Xray.X = "N" \
+        AND Dyspnoea.D = "N" \
+        AND Asia.A = Tuberculosis.A \
+        AND Smoking.S = Lungcancer.S \
+        AND Smoking.S = Bronchitis.S \
+        AND Tuberculosis.T = EitherTubOrLungcancer.T \
+        AND Lungcancer.L = EitherTubOrLungcancer.L \
+        AND Bronchitis.B = Dyspnoea.B \
+        AND EitherTubOrLungcancer.E = Dyspnoea.E \
+        AND EitherTubOrLungcancer.E = Xray.E \
+        GROUP BY HasB \
+        ORDER BY HasB' \
+        )
+
+    result = cr.fetchall()
+    desc = [d[0] for d in cr.description]
+    return (desc, result)
+
 
 
 db = create_database()
 insert_values(db)
-result = test_question(db)
-# result = ask_question(db)
+desc, result = test_question_bronchitis(db)
 
-print result
+print "##############################################"
+for d in desc:
+    print str(d) + "\t\t",
+print ""
+
+for r in result:
+    for x in r:
+        print str(x) + "\t\t",
+    print ""
+
 print ""
 
 n = result[0][1]
 y = result[1][1]
 
-print "Ponderated result (Y) = " + str(y / (y + n))
-print "Ponderated result (N) = " + str(n / (y + n))
+print "Ponderated result (Y) = {0:.10f}".format(y / (y + n)), 
+print " (N) = {0:.10f}".format(n / (y + n))
